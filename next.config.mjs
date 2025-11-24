aafdeb8b3defb506246d79bbb4a0df2eb4241639
Kushs1973 <99049891+Kushs1973@users.noreply.github.com>
2025-11-24 18:23:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This tells Vercel: "Don't try to compress this tool, run it raw."
-  serverExternalPackages: ['pdf-parse'], 
   eslint: {
     ignoreDuringBuilds: true,
   },
