a5e53d64ebff108c2b792ca7bb1eca2980d0b342
Kushs1973 <99049891+Kushs1973@users.noreply.github.com>
2025-11-24 14:09:59 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This tells Next.js to not try and bundle this library, 
-  // but to treat it as an external server tool.
-  serverExternalPackages: ['pdf-parse'],
-  
-  // This ignores strict type checking for the build to pass easier
+  // We removed the external packages rule so Vercel will now bundle the tool correctly.
   eslint: {
     ignoreDuringBuilds: true,
   },
